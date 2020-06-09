93fb16f100e9af44f81bac9dc90147959fcda41e
Kyle Williams <kylecharleswilliams@gmail.com>
2020-06-09 05:21:55 -0600

    Add location descriptions

---
+++ src/RestaurantData.js
@@ -0,0 +1,25 @@
+const restaurantData = {
+  stGermain: {
+    name: 'St. Germain',
+    address: '96 Boulevard Saint-Germain, 75005 Paris, France',
+    imageURL: '',
+    description: `St. Germain's Chipotle is located just three blocks from Notre-Dame de Paris. Le Jardin du Luxembourg is just three blocks away in the other direction. This location is the smallest of the three. 3.5 out of 5 stars.`,
+    nearbyLines: []
+  },
+  beaugrenelle: {
+    name: 'Beaugrenelle',
+    address: '2 Rue Linois, 75015 Paris, France',
+    imageURL: '',
+    description: `This Chipotle is located in the Beaugrenelle Mall and is hands-down the best Chipotle in Paris. It has something absolutely unheard of in France: A SODA FOUNTAIN. Yes, this Chipotle gives you a soda cup that you can refill to your heart's desire. No lukewarm cans of soda! Fill that cup to the BRIM with ice! AMERICA. The dining room is also the biggest of all other locations and has a beautiful view onto the Seine river right outside. Oh, it's also a ten minute walk from the Eiffel Tower. Try walking outside to the middle of the Grenelle bridge, there you can descend steps and walk on the Île aux Cygnes (Island of Swans) towards the Eiffel tower. It's an island in the middle of the Seine lined with trees; it's truly a magical experience and I did it every single Saturday. Go here if at all possible!  5 out of 5 stars.`,
+    nearbyLines: []
+  },
+  montmartre: {
+    name: 'Montmartre',
+    address: '18-20 Boulevard Montmartre, 75009 Paris, France',
+    imageURL: '',
+    description: `This was the first Chipotle I went to in Paris. It's located in an area of Paris that is known for its art and homeplace of Amélie! It's just a few blocks from Palais Garnier (Fantom of the Opera, anyone?). There are quite a few American fast-food chains around this location and it kind of takes you out of the Parisian experience. When I went it was the middle of June and I was thirsty, so I got two cans of Coca Light (Diet Coke). No regrets there. This location has two floors with a dining room on the second floor. 4 out of 5 stars.`,
+    nearbyLines: []
+  }
+}
+
+export default restaurantData;
--- src/components/RestaurantDetails/RestaurantDetails.js
+++ src/components/RestaurantDetails/RestaurantDetails.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 
-class RestaurantDetails extends Component { 
-  render() {
-    return(
-      <section>
-        <h2>Restaurant Details</h2>
-        <section>
-          Pics and stuff
-        </section>
-        <section>
-          Information and stuff
-        </section>
-        <NavLink to={`/navigate/${this.props.restaurantId}`}>
-          Navigate to this Location
-        </NavLink>
-      </section>
-    )
+const RestaurantDetails = (props) => {
+
+  const chooseRestaurant = () => {
+    
   }
+
+  return(
+    <section>
+      {chooseRestaurant()}
+      <h2>Restaurant Details</h2>
+      <section>
+        Pics and stuff
+      </section>
+      <section>
+        Information and stuff
+      </section>
+      <NavLink to={`/navigate/${props.restaurantId}`}>
+        Navigate to this Location
+      </NavLink>
+    </section>
+  )
 }
 
 export default RestaurantDetails;
